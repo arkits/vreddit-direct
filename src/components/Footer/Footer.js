fd0d5f2a238a6d8de8fdf72c7158c3d572ccd1d8
Archit Khode <arkits@outlook.com>
2019-05-20 20:26:20 -0700

    adding link to gh repo in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,22 +17,27 @@ class Footer extends Component {
         return (
             <div className="Footer">
                 <Container>
-                <Grid container spacing={1}>
-                    <Grid item xs={8}>
-                        <h4>
-                            vreddit-direct <br />
-                            <Link href="https://github.com/arkits" color="inherit">
-                                @arkits
-                            </Link>
-                        </h4>
+                    <Grid container spacing={1}>
+                        <Grid item xs={8}>
+                            <h4>
+                                <Link href="https://github.com/arkits/vreddit-direct" color="inherit">
+                                    vreddit-direct
+                                </Link>
+
+                                <br />
+
+                                <Link href="https://github.com/arkits" color="inherit">
+                                    @arkits
+                                </Link>
+                            </h4>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <h4>
+                                api_status: {this.props.api_status} <br />
+                                api_version: {this.props.api_version}
+                            </h4>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                        <h4>
-                        api_status: {this.props.api_status} <br />
-                        api_version: {this.props.api_version}
-                        </h4>
-                    </Grid>
-                </Grid>
                 </Container>
             </div>
         );
